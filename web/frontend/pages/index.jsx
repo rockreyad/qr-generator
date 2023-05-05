bd1ba8f7404ecb00f9d4bdcc56f9cd51ae8ccbe1
rockreyad <rockreyad@gmail.com>
2023-05-05 16:33:59 +0600

    shopify app init

---
+++ web/frontend/pages/index.jsx
@@ -0,0 +1,101 @@
+import {
+  Card,
+  Page,
+  Layout,
+  TextContainer,
+  Image,
+  Stack,
+  Link,
+  Text,
+} from "@shopify/polaris";
+import { TitleBar } from "@shopify/app-bridge-react";
+
+import { trophyImage } from "../assets";
+
+import { ProductsCard } from "../components";
+import { CalloutCard } from "@shopify/polaris";
+
+export default function HomePage() {
+  return (
+    <Page narrowWidth>
+      <TitleBar title="App name" primaryAction={null} />
+      <Layout>
+        <Layout.Section>
+          <CalloutCard
+            title="Customize the style of your checkout"
+            illustration="https://cdn.shopify.com/s/assets/admin/checkout/settings-customizecart-705f57c725ac05be5a34ec20c05b94298cb8afd10aac7bd9c7ad02030f48cfa0.svg"
+            primaryAction={{
+              content: "Customize checkout",
+              url: "#",
+            }}
+          >
+            <p>Upload your store’s logo, change colors and fonts, and more.</p>
+          </CalloutCard>
+        </Layout.Section>
+        <Layout.Section>
+          <Card sectioned>
+            <Stack
+              wrap={false}
+              spacing="extraTight"
+              distribution="trailing"
+              alignment="center"
+            >
+              <Stack.Item fill>
+                <TextContainer spacing="loose">
+                  <Text as="h2" variant="headingMd">
+                    Nice work on building a Shopify app 🎉
+                  </Text>
+                  <p>
+                    Your app is ready to explore! It contains everything you
+                    need to get started including the{" "}
+                    <Link url="https://polaris.shopify.com/" external>
+                      Polaris design system
+                    </Link>
+                    ,{" "}
+                    <Link url="https://shopify.dev/api/admin-graphql" external>
+                      Shopify Admin API
+                    </Link>
+                    , and{" "}
+                    <Link
+                      url="https://shopify.dev/apps/tools/app-bridge"
+                      external
+                    >
+                      App Bridge
+                    </Link>{" "}
+                    UI library and components.
+                  </p>
+                  <p>
+                    Ready to go? Start populating your app with some sample
+                    products to view and test in your store.{" "}
+                  </p>
+                  <p>
+                    Learn more about building out your app in{" "}
+                    <Link
+                      url="https://shopify.dev/apps/getting-started/add-functionality"
+                      external
+                    >
+                      this Shopify tutorial
+                    </Link>{" "}
+                    📚{" "}
+                  </p>
+                </TextContainer>
+              </Stack.Item>
+              <Stack.Item>
+                <div style={{ padding: "0 20px" }}>
+                  <Image
+                    source={trophyImage}
+                    alt="Nice work on building a Shopify app"
+                    width={120}
+                  />
+                </div>
+              </Stack.Item>
+            </Stack>
+          </Card>
+        </Layout.Section>
+        <Layout.Section>
+          <ProductsCard />
+        </Layout.Section>
+      </Layout>
+    </Page>
+  );
+}
